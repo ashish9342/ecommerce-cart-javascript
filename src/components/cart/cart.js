9b2a5d14eb674c2fbd06e4ea75cc0bd4a3833a65
Ashish Singh Rawat <ashish9342@gmail.com>
2020-10-13 17:22:59 +0530

    CSS fixed

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -18,7 +18,7 @@ function cart() {
     <p class="cart-total-title">${text.total}</p>
     <p class="">${text.items} : <span class="items-count">0</span>  </p>
     <p class="">${text.discount} : <span class="items-discount"> 0</span></p>
-    <p class="">${text.orderTotal} : <span class="cart-total-price">  $ 0</span><p>
+    <p class="bg-grey">${text.orderTotal} : <span class="cart-total-price">$ 0</span></p>
   </div>
 
 
